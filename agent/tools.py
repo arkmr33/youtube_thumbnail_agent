c72f8d23d37c613c19622b0089be0bb03e2e7b87
ambarkumar37 <kumarambar33@gmail.com>
2026-05-18 19:35:44 +0530

    updated tools

---
+++ agent/tools.py
@@ -0,0 +1,28 @@
+from langchain_community.tools.tavily_search import TavilySearchResults
+import os
+
+tavily = TavilySearchResults(
+    max_results=5,
+    tavily_api_key=os.getenv("TAVILY_API_KEY")
+)
+
+def web_search(state):
+    topic = state["topic"]
+
+    results = tavily.invoke(
+        {"query": f"YouTube thumbnail ideas for {topic}"}
+    )
+
+    summaries = []
+
+    for r in results:
+        title = r.get("title", "")
+        content = r.get("content", "")
+
+        summaries.append(
+            f"- {title}: {content[:200]}"
+        )
+
+    return {
+        "search_summary": "\n".join(summaries)
+    }
